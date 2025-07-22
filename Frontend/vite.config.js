a26e2da141c4aac303d7c6e4be77f4b4b4561682
Ketan Gulati <ketangulati66@gmail.com>
2025-07-23 00:59:39 +0530

    deployement prereq

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy:{
-      '/v1' : 'http://localhost:8000/api'
-    }
-  },
-  plugins: [react(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    server: {
+      proxy: {
+        '/v1': `${env.VITE_REACT_APP_BACKEND_BASEURL}/api`,
+      },
+    },
+    plugins: [react()],
+  }
 })
