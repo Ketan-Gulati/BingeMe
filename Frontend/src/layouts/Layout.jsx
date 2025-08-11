f15d13cc677cdd0b8131a9e1e9d055e01b05e1b4
Ketan Gulati <ketangulati66@gmail.com>
2025-08-11 11:43:39 +0530

    fixed dark mode inconsistency

--- Frontend/src/layouts/Layout.jsx
+++ Frontend/src/layouts/Layout.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import SidePanel from '../components/SidePanel'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header/Header";
+import SidePanel from "../components/SidePanel";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Layout() {
+  const theme = useSelector((state) => state.theme.theme);
+  const isDark = theme === "dark";
   return (
     <>
-        <div className='flex flex-col h-screen'>
-          <Header/>
-          <div className='flex flex-1 overflow-hidden'>
-            <SidePanel/>
-            <main className='flex-1 overflow-y-auto bg-[#1E1E1E]'>
-              <Outlet/>
-            </main>
-          </div>
+      <div className="flex flex-col h-screen">
+        <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <SidePanel />
+          <main
+            className={`flex-1 overflow-y-auto bg-[#1E1E1E] ${
+              isDark ? " text-white" : "bg-white text-gray-900"
+            }`}
+          >
+            <Outlet />
+          </main>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
